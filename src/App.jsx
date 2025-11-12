9450884544081300081b0dc44fd147917e0609e2
bcot-code <78989054+bcot-code@users.noreply.github.com>
2025-11-12 12:42:47 -0600

    adding app.jsx elements

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header.jsx';
+import SearchResults from './components/SearchResults.jsx';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <SearchResults />
+    </div>
+  )
+}
+
+export default App
