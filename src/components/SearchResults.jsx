c3b9a55f85fbc26a780d7eed0e0314182167e70b
bcot-code <78989054+bcot-code@users.noreply.github.com>
2025-11-13 21:41:35 -0600

    fixing the range and its css

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const SearchResults = () => {
+  const [price, setPrice] = useState(0);// State to hold the price range value
+
+  // Function to update the price value based on the range input
+  const updateValue = (value) => {
+    setPrice(value);
+  }
   return (
     <section className='search-results'>
     {/* HR Line Above Search Results */}
         <hr className='results-divider'/>
-        <h3>Search results for <span>"Fast"</span></h3>
+        <h3>Search results for <span>"Mercedes"</span></h3>
 
         <div className='price-range'>
             <label>Price by:</label>
-            <input type="range" min="0" max="100000" defaultValue="50000"/>
-            <span>$0 - $100,000</span>
+            <span>${price} - $100,000</span>
+            <input type="range" id="myRange" min="0" max="100000" value={price} onChange={(e) => updateValue(e.target.value)}/>
         </div>
 
         <div className='no-results'>
