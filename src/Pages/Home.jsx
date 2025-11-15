9c5f7a8fcda01e55959bb6c34c261c96d8bea5bb
bcot-code <78989054+bcot-code@users.noreply.github.com>
2025-11-15 15:51:10 -0600

    adding Homepage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import Header from '../components/Header.jsx';
-import FeaturedCars from '../components/FeaturedCars.jsx';
-import Categories from '../components/Categories.jsx';
-import SearchResults from '../components/SearchResults.jsx';
+import carImage from '../assets/chris-liverani-HUJDz6CJEaM-unsplash.jpg';
 import Footer from '../components/Footer.jsx';
 
 const Home = () => {
   return (
     <>
-        <Header />
-        <SearchResults />
-        <Categories />
-        <FeaturedCars />
-        <Footer />
+      <Header />
+      <section className='home-hero'>
+        <div className='hero-content'>
+            <h1>Find Your Dream Car</h1>
+            <p>Explore our extensive collection of vehicles and drive away in style.</p>
+        </div>
+        <div>
+            <img src={carImage} alt="Hero Car" className='hero-image'/>
+        </div>
+      </section>
+      <Footer />
     </>
   )
 }
