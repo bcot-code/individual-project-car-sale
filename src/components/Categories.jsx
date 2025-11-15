825ec2b4284fe66c023e6679faba0ae91b77821e
bcot-code <78989054+bcot-code@users.noreply.github.com>
2025-11-15 16:09:42 -0600

    init Categories, making filters

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import FeaturedCars from './FeaturedCars'
+import cars from '../data/cars.js';
 
 const Categories = () => {
+  const [filteredCars, setFilterCars] = useState(cars);
+  // Filter cars by bodyType
+  const filterCars = (type) => {
+    const filtered = cars.filter((car) => car.bodyType === type);
+    setFilterCars(filtered);
+  };
   return (
-    <div>Categories</div>
+    <section className='categories-section'>
+        <h2>Categories</h2>
+        <div className='categories-grid'>
+            {['Sedan', 'SUV', 'Sports', 'Electric', 'Truck'].map((type) => (
+                <button 
+                  key={type} 
+                  onClick={() => filterCars(type)}>{type}
+                </button>
+            ))}
+        </div>
+        {/* Featured Car based off selected categories */}
+        <FeaturedCars cars={filteredCars} />
+    </section>
   )
 }
 
