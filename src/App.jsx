669e648c7bae1af69ae56e62fe649e89ef3d6e10
bcot-code <78989054+bcot-code@users.noreply.github.com>
2025-11-15 16:02:03 -0600

    init Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home.jsx';
+import SearchResults from './Pages/SearchResults.jsx';
+import Contact from './Pages/Contact.jsx';
+
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+      {/* HOMEPAGE */}
         <Route path="/" element={<Home />} />
+      {/* SEARCH RESULTS PAGE */}
+        <Route path="/find-cars" element={<SearchResults />} />
+      {/* CONTACT PAGE */}
+        <Route path="/contact" element={<Contact />} />
       </Routes> 
     </BrowserRouter>
   )
