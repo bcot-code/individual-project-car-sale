c79a9e97bc13461de44c3a81664bb3d6c4cbfc18
bcot-code <78989054+bcot-code@users.noreply.github.com>
2025-11-15 15:52:15 -0600

    making the card stylish

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-const CarCard = () => {
+const CarCard = ({ car }) => {
   return (
-    <div>CarCard</div>
+    <div className='car-card'>
+        <img 
+          src={car.image} 
+          alt={`${car.make} ${car.model}`} 
+          className="car-image"
+        />
+        <div className="car-info">
+          <h3>{car.make} {car.model}</h3>
+          <p>{car.year} • {car.mileage} miles</p>
+          <p>${car.price.toLocaleString()}</p>
+          <button className="view-details">View Details</button>
+        </div>
+    </div>
   )
 }
 
